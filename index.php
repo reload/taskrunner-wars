ee5e801cad3d3f6b34858a81ce267e394bf0bdb5
Simon Larsson <simon@reload.dk>
2015-06-03 13:46:32 +0200

    add stuff

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <title>Test</title>
+  <link href="css/test.css" rel="stylesheet" title="Test">
+</head>
+<body>
+  <div class="page">
+    <h1>Test</h1>
+    <p>
+    Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+    </p>
+    <img src="http://cdn.osxdaily.com/wp-content/uploads/2013/07/dancing-banana.gif">
+  </div>
+</body>
+</html>
